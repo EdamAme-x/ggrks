f9a2ff40515a79ec1dcff05b0aa6319f85e6c4c7
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-07-14 06:02:53 +0000

    add copyright

--- ggrks/routes/index.tsx
+++ ggrks/routes/index.tsx
@@ -10,8 +10,9 @@ export default function Home() {
         <title>ggrks</title>
       </Head>
       <div class="px-4 py-8 mx-auto bg-[#86efac]">
-        <h1>~.deno.dev/[ググらせたい内容]</h1>
+        <h1>https://ggrks.deno.dev/[ググらせたい内容]</h1>
         <p>空白は+です。</p>
+        <p>by amex / <a href="https://twitter.com/macl2189">@macl2189</a></p>
       </div>
     </>
   );
